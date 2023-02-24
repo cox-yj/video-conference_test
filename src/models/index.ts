af842f55dd6749184a9f09e1306a2a577f901291
cox-yj <yuyongjun@talk-cloud.com>
2023-02-24 18:50:00 +0800

    *

---
+++ src/models/index.ts
@@ -0,0 +1,11 @@
+export interface User {
+    id: string;
+    name: string;
+    email: string;
+    avatar: string;
+    isOnline: boolean;
+    lastSeen: string;
+    createdAt: string;
+    updatedAt: string;
+  }
+  
---
+++ src/stores/index.ts
@@ -0,0 +1,9 @@
+import { userStore } from "./UserStore";
+import { roomStore } from "./RoomStore";
+// import 其他需要的 store
+
+export default {
+  userStore,
+  roomStore,
+  // 导出其他需要的 store
+};
