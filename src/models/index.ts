1653ac0cb2355411393978c0e86537432d894199
cox-yj <yuyongjun@talk-cloud.com>
2023-02-24 18:58:44 +0800

    *

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,11 +1,2 @@
-export interface User {
-    id: string;
-    name: string;
-    email: string;
-    avatar: string;
-    isOnline: boolean;
-    lastSeen: string;
-    createdAt: string;
-    updatedAt: string;
-  }
-  
+export * from './User';
+export * from './Room';
