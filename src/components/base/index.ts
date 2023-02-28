7bbb21faa1d63d755db9097da69bc5d0f7126989
nibohan <nibohan@talk-cloud.com>
2023-02-28 19:16:12 +0800

    feat(components): 注释无用引用

--- src/components/base/index.ts
+++ src/components/base/index.ts
@@ -2,8 +2,8 @@ import TKButton from './button/button'
 import TKConfigProvider from './config-provider'
 import TKInput from './input/input'
 import TKSelect from './select/select'
-import TKToast from './toast/toast'
+// import TKToast from './toast/toast'
 
 const prefixCls = 'tk-components'
 
-export { TKButton, TKConfigProvider, prefixCls, TKInput, TKSelect, TKToast }
+export { TKButton, TKConfigProvider, prefixCls, TKInput, TKSelect }
